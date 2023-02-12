abe121f0c7bb8059f9606945c6a46a9039fa1df1
Dragos Trandafir <dragostrandafir80@gmail.com>
2023-02-12 19:07:28 +0200

    Removed unnecesarry components

--- src/app/recipe-module/recipe-module.module.ts
+++ src/app/recipe-module/recipe-module.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { SharedModule } from '../shared/shared.module';
-import { NavbarComponent } from './navbar/navbar.component';
 import { CreateRecipeComponent } from './create-recipe/create-recipe.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RouterModule } from '@angular/router';
@@ -10,7 +9,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     RecipeListComponent,
-    NavbarComponent,
     CreateRecipeComponent,
     RecipeDetailComponent,
   ],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,7 +7,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
-import { AuthFormComponent } from './auth-form/auth-form.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
@@ -16,7 +15,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-  declarations: [CardComponent, AuthFormComponent],
+  declarations: [CardComponent],
   imports: [
     CommonModule,
     MatCardModule,
@@ -35,7 +34,6 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   ],
   exports: [
     CardComponent,
-    AuthFormComponent,
     MatCardModule,
     MatDividerModule,
     MatIconModule,
