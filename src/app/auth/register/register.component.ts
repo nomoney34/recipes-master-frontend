02405314103bced65be54668a463fd31a6689de4
nomoney34 <dragostrandafir80@gmail.com>
2024-06-04 23:45:07 +0300

    Added "confirm password" and fixed minor issues

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { MatDialogRef } from '@angular/material/dialog';
 
@@ -11,24 +11,77 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
 
+  passwordRequirements = [
+    { requirement: 'At least 8 characters', isMet: false },
+    { requirement: 'At least 1 uppercase letter', isMet: false },
+    { requirement: 'At least 1 lowercase letter', isMet: false },
+    { requirement: 'At least 1 number', isMet: false },
+    { requirement: 'At least 1 special character', isMet: false },
+  ];
+
   constructor(
-    private formBuilder: FormBuilder,
     public authService: AuthService,
-    public dialogRef: MatDialogRef<RegisterComponent>
+    public dialogRef: MatDialogRef<RegisterComponent>,
   ) {}
 
   ngOnInit(): void {
-    this.registerForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    });
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, this.passwordValidator.bind(this)]),
+      confirmPassword: new FormControl('', [Validators.required])
+    }, { validators: this.checkPasswords });
   }
-
+  
   register() {
     const email = this.registerForm.get('email')?.value;
     const password = this.registerForm.get('password')?.value;
 
-    this.authService.signUp(email, password);
+    this.authService.signUp(email, password).then(() => {
+      this.authService.signIn(email, password);
+    });
+
     this.dialogRef.close();
   }
+
+  passwordValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const password = control.value;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumeric = /[0-9]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password);
+    const hasMinLength = password.length >= 8;
+  
+    if (hasUpperCase && hasLowerCase && hasNumeric && hasSpecialChar && hasMinLength)  {
+      return null;
+    } else {
+      return { 'passwordStrength': true };
+    }
+  }
+
+
+  onPasswordInput(event: Event) {
+    const passwordInput = event.target as HTMLInputElement;
+    const password = passwordInput.value;
+    this.updatePasswordRequirements(password);
+    this.registerForm.updateValueAndValidity();
+  }
+
+  updatePasswordRequirements(password: string) {
+    this.passwordRequirements[0].isMet = password.length >= 8;
+    this.passwordRequirements[1].isMet = /[A-Z]/.test(password);
+    this.passwordRequirements[2].isMet = /[a-z]/.test(password);
+    this.passwordRequirements[3].isMet = /[0-9]/.test(password);
+    this.passwordRequirements[4].isMet = /[\W_]/.test(password);
+  }
+  
+  checkPasswords: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+    if(this.registerForm){
+      const password = this.registerForm.get('password')?.value;
+      const confirmPassword = this.registerForm.get('confirmPassword')?.value;
+      return password === confirmPassword ? null : { notSame: true };
+    
+    }
+    return { notSame: true };
+  };
+  
 }
